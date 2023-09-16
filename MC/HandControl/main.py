8e70529fa212704b48630610d008ecdd937b7ca6
fwalkowicz <walkowiczf@gmail.com>
2023-09-16 16:59:59 +0200

    add drawing landmarks

--- MC/HandControl/main.py
+++ MC/HandControl/main.py
@@ -1,49 +1,33 @@
-import os
-import cv2
 import time
-import mediapipe as mp
-import numpy as np
+import cv2
+import math
+from DetectHand import HandDetector
 
-# Menu setting
-header_folder = 'header'
-header_folder_path = os.path.join(os.getcwd(), header_folder)
-header_image = cv2.imread(os.path.join(header_folder_path, 'menu.png'))
-header_image = cv2.resize(header_image, (1280, header_image.shape[0]))
+if __name__ == "__main__":
+    end_time = 0
+    hand = HandDetector(False, 0.5, 0.5)
+    camera = cv2.VideoCapture(0)
+    while True:
+        ret, frame = camera.read()
+        hand.find_hands(image=frame)
+        left_landmarks, right_landmarks = hand.find_landmark_position(frame, 0)
+        if len(left_landmarks) != 0:
+            cv2.circle(frame, (left_landmarks[8][2], left_landmarks[8][3]), 5, (255, 0, 0), cv2.FILLED)
 
-# camera settings
-camera = cv2.VideoCapture(0)
-camera.set(3, 1280)
-camera.set(4, 720)
+        if len(right_landmarks) != 0:
+            cv2.circle(frame, (right_landmarks[4][2], right_landmarks[4][3]), 5, (0, 255, 255), cv2.FILLED)
+            cv2.circle(frame, (right_landmarks[8][2], right_landmarks[8][3]), 5, (0, 255, 255), cv2.FILLED)
+            cv2.putText(frame, str(f'x: {right_landmarks[8][2]}'), (20, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
+            cv2.circle(frame, (right_landmarks[12][2], right_landmarks[12][3]), 5, (0, 255, 255), cv2.FILLED)
+            cv2.putText(frame, str(f'y: {right_landmarks[8][3]}'), (150, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
+            cv2.line(frame, (right_landmarks[4][2], right_landmarks[4][3]), (right_landmarks[8][2], right_landmarks[8][3]), (255, 0, 128), 3)
+            distance = math.hypot(right_landmarks[8][2] - right_landmarks[4][2], right_landmarks[8][3] - right_landmarks[4][3])
+            cv2.putText(frame, f"Distance: {int(distance)}", (280, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
 
-# Hand detection module
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(static_image_mode=False,
-                      max_num_hands=2,
-                      min_detection_confidence=0.5,
-                      min_tracking_confidence=0.5)
-mpDraw = mp.solutions.drawing_utils
-
-
-while True:
-    # read frame from camera
-    ret, frame = camera.read()
-
-    # set menu into frame from camera
-    frame[0:125] = cv2.addWeighted(frame[0:125], 0.5, header_image, 0.7, 0)
-
-    # detect hand from frame
-    imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-
-    # draw hand landmarks
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate( handLms.landmark):
-                h, w, c = frame.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-
-            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
-
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+        start_time = time.time()
+        fps = 1 / (start_time - end_time)
+        end_time = start_time
+        cv2.putText(frame, f"fps: {int(fps)}", (20, 40), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
