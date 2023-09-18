cfd40937b5113f87c51f71dd178bdcd987476379
fwalkowicz <walkowiczf@gmail.com>
2023-09-18 08:18:43 +0200

    update hand gesture detection

--- MC/HandControl/DetectGesture.py
+++ MC/HandControl/DetectGesture.py
@@ -1,15 +1,65 @@
 import os.path
-
 import cv2
-import mediapipe as mp
+import math
+import numpy as np
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import tensorflow as tf
 
 
 class HandGesture:
     def __init__(self):
         self.image_dir = os.path.join(os.getcwd(), 'HandGesture')
+        self.detector = HandDetector(maxHands=2)
+        self.classifier = Classifier('/home/filip/PycharmProjects/Comcore_Sensors/MC/HandControl/Models/keras_model.h5', '/home/filip/PycharmProjects/Comcore_Sensors/MC/HandControl/Models/labels.txt')
+        self.interpreter = tf.lite.Interpreter(model_path='/home/filip/PycharmProjects/Comcore_Sensors/MC/HandControl/Models/model_unquant.tflite')
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
 
-    def image_processing(self):
-        image_list = os.listdir(self.image_dir)
+    def collect_data(self, image):
+        hands, img = self.detector.findHands(image)
+        img_white = np.ones((224, 224, 3), np.uint8)
+        img_white = cv2.bitwise_not(img_white)
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand['bbox']
+            crop_image = img[y - 20: y + h + 20, x - 20: x + w + 20]
+            aspect_ratio = h/w
+            if aspect_ratio > 1:
+                k = 224 / h
+                w_cal = math.ceil(k * w)
+                img_resize = cv2.resize(crop_image, (w_cal, 224))
+                # Resize img_resize to match the shape of img_white
+                img_resize = cv2.resize(img_resize, (img_white.shape[1], img_white.shape[0]))
+                img_white[0:img_resize.shape[0], 0:img_resize.shape[1]] = img_resize
+
+        return img_white
+
+    def predict_gesture_tensorflow(self, image):
+        image_prediction = self.collect_data(image)
+        prediction, index = self.classifier.getPrediction(image_prediction)
+
+        return prediction, index
+
+    def predict_gesture_tensorflow_lite(self, image):
+        image_prediction = self.collect_data(image)
+        image_prediction = cv2.cvtColor(image_prediction, cv2.COLOR_BGR2GRAY)
+        input_data = cv2.resize(image_prediction, (224, 224))
+        input_data_rgb = cv2.cvtColor(input_data, cv2.COLOR_GRAY2RGB)
+        input_data_rgb = input_data_rgb.astype(np.float32) / 255.0
+        input_data_rgb = np.expand_dims(input_data_rgb, axis=0)
+        self.interpreter.set_tensor(self.input_details[0]['index'], input_data_rgb)
+        self.interpreter.invoke()
+        output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+        class_predictions = np.argmax(output_data)
+        class_labels = [2, 1, 0]
+
+        # Get the predicted class label
+        predicted_class = class_labels[class_predictions]
+
+        results = {'id': predicted_class}
+        return results
 
     @staticmethod
     def finger_counter(lm_list):
@@ -29,6 +79,3 @@ class HandGesture:
 
         return gesture_list
 
-
-if __name__ == "__main__":
-    pass
--- MC/HandControl/main.py
+++ MC/HandControl/main.py
@@ -4,49 +4,70 @@ import math
 from DetectHand import HandDetector
 from DetectGesture import HandGesture
 import numpy as np
+import cvzone
+import os
 
 if __name__ == "__main__":
     end_time = 0
     arm_range = 0
     arm_range_per = 0
+    classes = ['MoveX', 'MoveY', 'MoveGrip']
     hand = HandDetector(False, 0.5, 0.5)
     gesture = HandGesture()
     camera = cv2.VideoCapture(0)
     while True:
         ret, frame = camera.read()
         hand.find_hands(image=frame)
+        img = gesture.collect_data(frame.copy())
+        # prediction, index = gesture.predict_gesture_tensorflow(frame.copy())
+        results = gesture.predict_gesture_tensorflow_lite(frame.copy())
+        index = results['id']
         left_landmarks, right_landmarks = hand.find_landmark_position(frame, 0)
+        cv2.putText(frame, f"Detection: {classes[index]}", (20, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
         if len(left_landmarks) != 0:
             cv2.circle(frame, (left_landmarks[8][1], left_landmarks[8][2]), 5, (255, 0, 0), cv2.FILLED)
 
         if len(right_landmarks) != 0:
             cv2.circle(frame, (right_landmarks[4][1], right_landmarks[4][2]), 5, (0, 255, 255), cv2.FILLED)
             cv2.circle(frame, (right_landmarks[8][1], right_landmarks[8][2]), 5, (0, 255, 255), cv2.FILLED)
-            cv2.putText(frame, str(f'x: {right_landmarks[8][1]}'), (20, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
+            if classes[index] == classes[0]:
+                cv2.putText(frame, str(f'x: {right_landmarks[8][1]}'), (20, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
             cv2.circle(frame, (right_landmarks[12][1], right_landmarks[12][2]), 5, (0, 255, 255), cv2.FILLED)
-            cv2.putText(frame, str(f'y: {right_landmarks[8][2]}'), (150, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
-            cv2.line(frame, (right_landmarks[4][1], right_landmarks[4][2]), (right_landmarks[8][1], right_landmarks[8][2]), (255, 0, 128), 3)
+            if classes[index] == classes[1]:
+                cv2.putText(frame, str(f'y: {right_landmarks[8][2]}'), (20, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
             distance = math.hypot(right_landmarks[8][1] - right_landmarks[4][1], right_landmarks[8][2] - right_landmarks[4][2])
-            cv2.putText(frame, f"Distance: {int(distance)}", (280, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
-
+            if classes[index] == classes[2]:
+                cv2.line(frame, (right_landmarks[4][1], right_landmarks[4][2]),(right_landmarks[8][1], right_landmarks[8][2]), (255, 0, 128), 3)
+                cv2.putText(frame, f"Distance: {int(distance)}", (20, 80), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
             finger_counter = gesture.finger_counter(right_landmarks)
-            cv2.putText(frame, f"Fingers: {finger_counter}", (20, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
+            cv2.putText(frame, f"Fingers: {finger_counter}", (140, 40), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
 
             # range 0 - 100
-            arm_range = np.interp(distance, [20, 200], [400, 150])
-            arm_range_per = (arm_range - 150) / (400 - 150) * 100
+            arm_range = np.interp(distance, [20, 200], [400, 180])
+            arm_range_per = (arm_range - 180) / (400 - 180) * 100
 
-        cv2.rectangle(frame, (50, 150), (85, 400), (0, 255, 0), 3)
-        cv2.rectangle(frame, (50, 150), (85, int(arm_range)), (0, 255, 0), cv2.FILLED)
-        cv2.putText(frame, f"{int(arm_range_per)}  %", (20, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
-
-
+        if classes[index] == classes[2]:
+            cv2.rectangle(frame, (50, 180), (85, 400), (0, 255, 0), 3)
+            cv2.rectangle(frame, (50, 180), (85, int(arm_range)), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, f"{int(arm_range_per)}  %", (20, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
 
         start_time = time.time()
         fps = 1 / (start_time - end_time)
         end_time = start_time
         cv2.putText(frame, f"fps: {int(fps)}", (20, 40), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+        # Create a list of all processed images
+        imgList = [frame, img]
+        stackedImg = cvzone.stackImages(imgList, 2, 1)
 
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame', stackedImg)
         if cv2.waitKey(1) == ord('q'):
             break
+
+        """
+        if cv2.waitKey(1) == ord('x'):
+            cv2.imwrite(f"HandGesture/HandX/hand_{time.time()}.jpg", img)
+        if cv2.waitKey(1) == ord('y'):
+            cv2.imwrite(f"HandGesture/HandY/hand{time.time()}.jpg", img)
+        if cv2.waitKey(1) == ord('g'):
+            cv2.imwrite(f"HandGesture/HandGrip/hand{time.time()}.jpg", img)
+        """
